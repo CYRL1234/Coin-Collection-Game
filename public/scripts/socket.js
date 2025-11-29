8afe0f5ae1d27af8b1bec1a31724100520fe0798
CYRL1234 <krlieu@ust.hk>
2025-11-30 01:05:42 +0800

    Add files via upload

---
+++ public/scripts/socket.js
@@ -0,0 +1,151 @@
+const Socket = (function() {
+    // This stores the current Socket.IO socket
+    let socket = null;
+
+    // This function gets the socket from the module
+    const getSocket = function() {
+        return socket;
+    };
+
+    // This function connects the server and initializes the socket
+    const connect = function() {
+        socket = io();
+
+        // Wait for the socket to connect successfully
+        socket.on("connect", () => {
+            // Get the online user list
+            socket.emit("get users");
+
+            // Get the chatroom messages
+            socket.emit("get messages");
+        });
+
+        // Set up the users event
+        socket.on("users", (onlineUsers) => {
+            onlineUsers = JSON.parse(onlineUsers);
+
+            // Show the online users
+            OnlineUsersPanel.update(onlineUsers);
+        });
+
+        // Set up the add user event
+        socket.on("add user", (user) => {
+            user = JSON.parse(user);
+
+            // Add the online user
+            OnlineUsersPanel.addUser(user);
+        });
+
+        // Set up the remove user event
+        socket.on("remove user", (user) => {
+            user = JSON.parse(user);
+
+            // Remove the online user
+            OnlineUsersPanel.removeUser(user);
+        });
+
+
+        socket.on("start response",() => {
+            //start the game
+            if(gammingpanel.getCanPlay()){
+                gammingpanel.startGame();
+            }
+        });
+
+        socket.on("full", () =>{
+            $("#full").text("FULL");
+        });
+
+        socket.on("your player id", (id) =>{
+            //console.log(id);
+            gammingpanel.setCanPlay(true);
+            gammingpanel.setPlayerId(id);
+        });
+        socket.on("playerposition",(position)=> {
+            position = JSON.parse(position);
+            gammingpanel.setPlayerchange(position.playerId)
+            gammingpanel.setplayerx(position.x)
+            gammingpanel.setplayery(position.y)
+        } )
+
+        socket.on("gem_collected",(data) => {
+            data = JSON.parse(data)
+            const {gemId} = data.gemId;
+            gammingpanel.gemCollected(gemId);
+            const k = data.k;
+            gammingpanel.setk(k);
+        })
+        socket.on("winner",(data)=>{
+            winner = JSON.parse(data)
+            gammingpanel.setwinner(winner.winner);
+        })
+        socket.on("player_teleported",(data) => {
+            const{playerId,targetx,targety} = JSON.parse(data);
+            gammingpanel.TP(playerId,targetx,targety);
+        })
+    };
+
+    // This function disconnects the socket from the server
+    const disconnect = function() {
+        if(socket){
+            socket.disconnect();
+            socket = null;
+        }
+    };
+
+    // This function sends a post message event to the server
+    const postMessage = function(content) {
+        if (socket && socket.connected) {
+            socket.emit("post message", content);
+        }
+    };
+
+    const typing = function(){
+        if (socket && socket.connected) {
+            socket.emit("typing");
+        }
+    };
+    const playermove = function(content){
+        if (socket && socket.connected) {
+            console.log(content.x);
+            socket.emit("player_move",content);
+        }
+    };
+
+    const requestStart = function(){
+        if (socket && socket.connected) {
+            console.log("socket.requestStart")
+            socket.emit("request_start");
+        }
+    };
+
+    const sendEndGame = function(){
+        if (socket && socket.connected) {
+            console.log("sendEndGame")
+            socket.emit("endGame");
+        }
+    }
+
+    const playerCollectGem = function(playerId, gemId){
+        if (socket && socket.connected) {
+            //console.log("playerCollectGem")
+            socket.emit("playerCollectGem", JSON.stringify({playerId: playerId,
+                gemId: gemId})
+
+            );
+        }
+    };
+    const playerTeleport = function(playerId,targetx,targety){
+        if (socket && socket.connected) {
+            socket.emit("playerTeleport", JSON.stringify({
+                playerId: playerId,
+                targetx: targetx,
+                targety:targety
+            }));
+        }
+    }
+
+    return { getSocket, connect, disconnect, postMessage,
+         typing ,playermove, requestStart, sendEndGame, playerCollectGem,playerTeleport};
+})();
+
---
+++ public/scripts/ui.js
@@ -0,0 +1,554 @@
+const SignInForm = (function() {
+    // This function initializes the UI
+    const initialize = function() {
+        // Populate the avatar selection
+        console.log("hi 1")
+        // Hide it
+        $("#signin-overlay").hide();
+
+        // Submit event for the signin form
+        $("#signin-form").on("submit", (e) => {
+            // Do not submit the form
+            e.preventDefault();
+
+            // Get the input fields
+            const username = $("#signin-username").val().trim();
+            const password = $("#signin-password").val().trim();
+            console.log("check pt3");
+            // Send a signin request
+            Authentication.signin(username, password,
+                () => {
+                    hide();
+                    //UserPanel.update(Authentication.getUser());
+                    //UserPanel.show();
+                    $(".signout-button").prop('disabled', false);
+                    Socket.connect();
+                    $(".start-btn").prop('disabled', false);
+                },
+                (error) => { $("#signin-message").text(error); }
+            );
+        });
+
+        // Submit event for the register form
+        $("#register-form").on("submit", (e) => {
+            // Do not submit the form
+            e.preventDefault();
+
+            // Get the input fields
+            const username = $("#register-username").val().trim();
+            const password = $("#register-password").val().trim();
+            const confirmPassword = $("#register-confirm").val().trim();
+
+            // Password and confirmation does not match
+            if (password != confirmPassword) {
+                $("#register-message").text("Passwords do not match.");
+                return;
+            }
+
+            // Send a register request
+            Registration.register(username, password,
+                () => {
+                    $("#register-form").get(0).reset();
+                    $("#register-message").text("You can sign in now.");
+                },
+                (error) => { $("#register-message").text(error); }
+            );
+        });
+    };
+
+    // This function shows the form
+    const show = function() {
+        $("#front-page-container").fadeIn(500);
+        $("#signin-overlay").fadeIn(500);
+    };
+
+    // This function hides the form
+    const hide = function() {
+        $("#signin-form").get(0).reset();
+        $("#signin-message").text("");
+        $("#register-message").text("");
+        $("#signin-overlay").fadeOut(500);
+    };
+
+    return { initialize, show, hide };
+})();
+
+
+const gameStartPanel = (function() {
+    
+    const initialize = function() {
+        console.log("hi 2");
+        $(".signout-button").on("click", () => {
+            // Send a signout request
+            Authentication.signout(
+                () => {
+                    Socket.disconnect();
+                    SignInForm.show();
+                    $(".signout-button").prop("disabled", true); // Use prop instead of disabled
+                    $("#log_in_successful").text("");
+                }
+            );
+        });
+        $(".start-btn").on("click", () => {
+            $(".start-btn").prop('disabled', true);
+            $(".start-btn").text("Waiting")
+            console.log("Press start button");
+            Socket.requestStart();
+            console.log(gammingpanel.getPlayerId())
+            ////
+        });
+    };
+
+
+    const show = function() {
+        $("#game-start-panel").fadeIn(500);
+    };
+
+    const hide = function() {
+        $("#log_in_successful").text("");
+        $("#game-start-panel").fadeOut(500);
+    };
+
+    return { initialize, show, hide };
+})();
+
+const gameOverPanel = (function() {
+    const initialize = function() {
+        $(".signout-button").on("click", () => {
+            // Send a signout request
+            Authentication.signout(
+                () => {
+                    hide();
+                    Socket.disconnect();
+                    SignInForm.show();
+                    $(".signout-button").prop("disabled", true); // Use prop instead of disabled
+                    $("#log_in_successful").text("");
+                }
+            );
+        });
+        $(".start-btn").on("click", () => {
+            $(".start-btn").prop('disabled', true);
+            $(".start-btn").text("Waiting")
+            console.log("Press start button");
+            Socket.requestStart();
+            console.log("request successful")
+            console.log(gammingpanel.getPlayerId())
+            ////
+        });
+    };
+
+    const handleEndGame = function(){
+
+        Socket.sendEndGame();
+        setTimeout(() => {
+            gammingpanel.hide();
+            show();
+        },500); 
+    }
+
+    const show = function() {
+        console.log(gammingpanel.getwinner());
+        console.log(gammingpanel.getPlayerId());
+        if(gammingpanel.getwinner()==gammingpanel.getPlayerId()){
+            $('#result-status').text('Win'); 
+            //const users = JSON.parse(fs.readFileSync("./data/users.json"));
+        }
+        else if(gammingpanel.getwinner()==2){
+            $('#result-message').text('Draw');
+            $('#result-status').text('Game');
+        }
+        else{
+            $('#result-status').text('Loose');
+        }
+        $("#game-over-container").fadeIn(500);
+    };
+
+    const hide = function() {
+        $("#game-over-container").fadeOut(500);
+    };
+
+    return { initialize, show, hide, handleEndGame };
+})();
+
+const gammingpanel = (function() {
+    let playerId = -1;
+    let player_change = -1;
+    let player_x = -1;
+    let player_y = -1;
+    let canPlay = false;
+    let gameStart = false;
+    let gemIdchange = -1;
+    let k = -1;
+    let winner = -1;
+    let tp = -1;
+    const sounds = {
+        background: new Audio("background.mp3"),
+        collect: new Audio("collect.mp3"),
+        gameover: new Audio("gameover.mp3")
+    };
+    const initialize = function(){
+        $("#game-container").on("mousedown", function(event) {
+            /* 获取鼠标点击位置 */
+            let mouseX = event.clientX; // 获取鼠标的 X 坐标
+            let mouseY = event.clientY; // 获取鼠标的 Y 坐标
+            /* 处理鼠标按下，移动玩家到点击位置 */
+            //player1.moveTo(mouseX, mouseY); // 假设 player1 有 moveTo 方法
+            // io.exit()
+            const content = {
+                playerId:getPlayerId(),
+                x: mouseX,
+                y: mouseY,
+
+            };
+            Socket.playermove(content);
+        });
+    };
+    const setk = function(id){
+        k = id;
+    }
+    const getk = function(){
+        return k;
+    }
+    const setwinner = function(id){
+        winner = id;
+    }
+    const getwinner = function(){
+        return winner;
+    }
+    const setPlayerId = function(id){
+        playerId = id;
+        //console.log(playerId);
+    }
+    const setPlayerchange = function(id){
+        player_change = id;
+    }
+    const TP = function(playerId,targetx,targety){
+        switch(playerId){
+            case 0: 
+                tp = 0;
+                player_x = targetx;
+                player_y = targety;
+                break;
+            case 1:
+                tp = 1;
+                player_x = targetx;
+                player_y = targety;
+        }
+    }
+    const setplayerx = function(id){
+        player_x = id;
+    }
+    const setplayery= function(id){
+        player_y = id;
+    }
+    const setgemchange= function(id){
+        gemIdchange = id;
+    }
+    const getPlayerchange = function(){
+        return player_change;
+    }
+    const getplayerx = function(){
+        return player_x;
+    }
+    const getplayery= function(){
+        return player_y;
+    }
+    const getgemchange= function(){
+        return gemIdchange;
+    }
+    const getGameStart = function(){
+        return gameStart;
+    }
+    
+    const gemCollected = function(gemId){
+        gemIdchange = gemId;
+    }
+    const startGame = function(){
+        sounds.background.play();
+        gameStart = true;
+        $(".start-btn").prop('disabled', false);
+        $(".start-btn").text("Start");
+        gameOverPanel.hide();
+        $("#front-page-container").fadeOut(500);
+
+        $("#game-container").fadeIn(500);
+        let countdownStart = 4; // 从 3 开始倒计时
+        countdown();
+        function countdown() {
+            // Decrease the remaining time
+            countdownStart = countdownStart -1;
+            $("#game-title").text(countdownStart);
+            // Continue the countdown if there is still time;
+            if(countdownStart>0){
+                setTimeout(countdown,1000);
+            }
+            // otherwise, start the game when the time is up
+            else{
+                $("#game-title").text("GO")
+                clearInterval(countdown); // 清除定时器
+                $("#game-title").fadeOut(500);
+            }
+        }
+        // 使用 setTimeout 延迟执行下一步
+        setTimeout(() => {
+            $("#game-start").hide();
+        }, 3000); // 延迟 3000 毫秒（3 秒）
+
+        const canvas = document.getElementById("gameCanvas");
+        const context = canvas.getContext("2d");
+        const gameArea = BoundingBox(context, 0, 0, 420, 800);
+
+        let  walls = [BoundingBox(context, 0, 0, 420, 5),
+            BoundingBox(context, 200, 10, 210, 110),
+            BoundingBox(context, 100, 100, 200, 110),
+            BoundingBox(context, 0, 500, 200, 510),
+            BoundingBox(context, 190, 300, 200, 500),
+            BoundingBox(context, 300, 600, 310, 800),
+            BoundingBox(context, 100, 590, 310, 600),
+            BoundingBox(context, 300, 300, 420, 310),
+            BoundingBox(context, 300, 100, 310, 300),
+            BoundingBox(context, 0, 795, 420, 800),
+            BoundingBox(context, 0, 0, 5, 800),
+            BoundingBox(context, 415, 0, 420, 800),
+            
+        ];
+        let gameStartTime = 0;   
+        const totalGameTime = 60; 
+        let player1 = Player(context, 427, 240, gameArea, walls); // 玩家 1
+        let player2 = Player2(context, 427, 240, gameArea, walls); // 玩家 2
+        let gems = [Gem(context, 427, 350, "green")];
+        const cornerPoints = gameArea.getPoints();  
+        let collectedGems = 0;
+        let fires = [
+            Fire(context, cornerPoints.topLeft[0], cornerPoints.topLeft[1], "realfire"),
+            Fire(context, cornerPoints.topRight[0], cornerPoints.topRight[1], "realfire"), 
+            Fire(context, cornerPoints.bottomLeft[0], cornerPoints.bottomLeft[1], "realfire"), 
+            Fire(context, cornerPoints.bottomRight[0], cornerPoints.bottomRight[1], "realfire")
+        ];
+        portal1 = Portal(context,60,60,120,60);
+        portal2 = Portal(context,790,410,730,410);
+        setTimeout(() => {
+            drawGame(0); 
+        }, 3000); // 延迟 3000 毫秒（3 秒）   
+        // socket.onmessage = function(event) {
+        //     const data = JSON.parse(event.data);
+        //     const players = data.players;
+        //     Object.assign(players, {
+        //         player1: { name: "Alice", score: 0 },
+        //         player2: { name: "Bob", score: 0 }
+        //     });
+        //     drawGame(0);
+        // };
+
+        function drawGame(now) {
+            if (gameStartTime == 0) gameStartTime = now;
+            const gameTimeSoFar = now - gameStartTime;
+            const timeRemaining = Math.ceil((totalGameTime * 1000 - gameTimeSoFar) / 1000);
+            $("#time-remaining").text(timeRemaining);
+            if(timeRemaining == 0){
+                gameStart = false;
+                hide();
+                sounds.background.pause();
+                gameOverPanel.handleEndGame();
+                return
+            }
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            for (let i = 0; i < walls.length; i++) {
+                context.fillStyle = 'white'; // 设置填充颜色
+                    const box = walls[i].draw()
+                    context.fillRect(box[0],box[1],box[2],box[3]); // 绘制墙壁
+            }
+
+            gems.forEach(gem => {
+                if (!gem.collected) {
+                    gem.update(now);
+                    gem.draw();
+                }
+            });
+            //console.log(getPlayerchange());
+            if(getPlayerchange()!=-1){
+                if(getPlayerchange()==0){
+                    player1.moveTo(getplayerx(),getplayery())
+                    setPlayerchange(-1)
+                }
+                else{
+                    player2.moveTo(getplayerx(),getplayery())
+                    setPlayerchange(-1)
+                }
+            }
+            if(getgemchange()!=-1){
+                //console.log(getgemchange());
+                fires.forEach(f => f.draw());
+                console.log(getk());
+                gems[0].randomize(getk());
+                //gems[getgemchange()].randomize(gameArea);
+                setgemchange(-1);
+            }
+            player1.update(now);
+            player1.draw();
+            player2.update(now);
+            player2.draw();
+            portal1.update(now);
+            portal1.draw();
+            portal2.update(now);
+            portal2.draw();
+            let portal1Position = portal1.getXY();
+            let portal1_x = portal1Position.x;
+            let portal1_y = portal1Position.y;
+            let portal2Position = portal2.getXY();
+            let portal2_x = portal2Position.x;
+            let portal2_y = portal2Position.y;
+            switch (playerId){
+                case 0:
+                let playerBox = player1.getBoundingBox();
+                let portal_1_intersect = playerBox.isPointInBox(portal1_x,portal1_y);
+                if(portal_1_intersect == true){
+                    //
+                    Socket.playerTeleport(playerId,portal2.targetx,portal2.targety);
+                }
+                let portal_2_intersect = playerBox.isPointInBox(portal2_x,portal2_y);
+                if(portal_2_intersect == true){
+                    //
+                    Socket.playerTeleport(playerId,portal1.targetx,portal1.targety);
+                }
+                
+                for (i = 0; i < gems.length; i = i + 1){
+                    let gemPosition = gems[i].getXY();
+                    let gem_x = gemPosition.x;
+                    let gem_y = gemPosition.y;
+                    let intersect = playerBox.isPointInBox(gem_x,gem_y);
+                    gemId = i;
+                    //console.log(gemId);
+                    if(intersect == true){
+                        sounds.collect.currentTime = 0;
+                        sounds.collect.play();
+                        Socket.playerCollectGem(playerId, gemId);
+                        let currentHP = parseInt($('#HP').text(), 10);
+                        currentHP+=1;
+                        console.log(currentHP);
+                        $('#HP').text(currentHP);
+                        //sounds.collect.currentTime = 0;
+                        //sounds.collect.play();
+                        //gem.randomize(gameArea);
+                    }
+                }
+                    break
+                case 1:
+                    let playerBox2 = player2.getBoundingBox();
+                    for (i = 0; i < gems.length; i = i + 1){
+                        let gemPosition = gems[i].getXY();
+                        let gem_x = gemPosition.x;
+                        let gem_y = gemPosition.y;
+                        let intersect = playerBox2.isPointInBox(gem_x,gem_y);
+                        gemId = i;
+                        //console.log(gemId);
+                        if(intersect == true){
+                            sounds.collect.currentTime = 0;
+                            sounds.collect.play();
+                            Socket.playerCollectGem(playerId, gemId);
+                            let currentHP = parseInt($('#HP').text(), 10);
+                            currentHP+=1;
+                            console.log(currentHP);
+                            $('#HP').text(currentHP);
+                            //sounds.collect.currentTime = 0;
+                            //sounds.collect.play();
+                            //gem.randomize(gameArea);
+                        }
+                    }
+                    let portal_1_intersect_2 = playerBox2.isPointInBox(portal1_x,portal1_y);
+                    if(portal_1_intersect_2 == true){
+                        //
+                        Socket.playerTeleport(playerId,portal2.targetx,portal2.targety);
+                    }
+                    let portal_2_intersect_2 = playerBox2.isPointInBox(portal2_x,portal2_y);
+                    if(portal_2_intersect_2 == true){
+                        //
+                        Socket.playerTeleport(playerId,portal1.targetx,portal1.targety);
+                    }
+                    break
+            }
+            if(tp == 0){
+                tp = -1
+                player1.teleport(player_x,player_y);
+                player_x = -1
+                player_y = -1;
+            }
+            if(tp == 1){
+                tp = -1
+                player2.teleport(player_x,player_y);
+                player_x = -1
+                player_y = -1;
+            }
+            fires.forEach(f => f.update(now));
+            fires.forEach(f => f.draw());
+
+            requestAnimationFrame(drawGame); // 循环调用绘制函数
+        }
+
+        // 当游戏开始按钮被点击时
+        // $("#game-start").on("click", function() {
+        //     $("#game-start").hide(); // 隐藏开始界面
+        //     // 游戏逻辑开始
+        //     $("#game-container").on("mousedown", function(event) {
+        //         const mouseX = event.clientX; // 获取鼠标点击位置
+        //         const mouseY = event.clientY; // 获取鼠标的 Y 坐标
+        //         // if(playerId==0){
+        //         //     player1.moveTo(mouseX, mouseY);
+        //         // }
+        //         // else{
+        //         //     player2.moveTo(mouseX, mouseY); // 假设 player1 有 moveTo 方法
+        //         // }
+        //         // player1.moveTo(mouseX, mouseY); // 假设 player1 有 moveTo 方法
+        //     });
+        //     requestAnimationFrame(drawGame); // 开始游戏的绘制循环
+        // });
+    };
+
+    const setCanPlay = function(play){
+        canPlay = play;
+    }
+    const getPlayerId = function(){
+        return playerId;
+    }
+    const getCanPlay = function(){
+        return canPlay;
+    }
+    const show = function() {
+        $("#game-container").fadeIn(500);
+    };
+
+    const hide = function() {
+        $("#game-container").fadeOut(500);
+    };
+    return { initialize, show, hide, setPlayerId,startGame,setCanPlay,getCanPlay,getPlayerId
+        ,setPlayerchange,setplayerx ,setplayery,getPlayerchange,getplayerx,getplayery, getGameStart, gemCollected
+        ,setgemchange,getgemchange,setk,getk,setwinner,getwinner,TP
+     };
+}
+
+)();
+
+
+
+const UI = (function() {
+    // This function gets the user display
+    const getUserDisplay = function(user) {
+        return $("<div class='field-content row shadow'></div>")
+            .append($("<span class='user-avatar'>" +
+			        Avatar.getCode(user.avatar) + "</span>"))
+            .append($("<span class='user-name'>" + user.name + "</span>"));
+    };
+
+    // The components of the UI are put here
+    const components = [SignInForm, gameStartPanel, gameOverPanel,gammingpanel];
+
+    // This function initializes the UI
+    const initialize = function() {
+        // Initialize the components
+        for (const component of components) {
+            component.initialize();
+        }
+    };
+
+    return { getUserDisplay, initialize };
+})();
